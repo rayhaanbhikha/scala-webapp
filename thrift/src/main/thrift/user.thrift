8fb6a8ec68191339a8c83dcab7b60f78620c3a13
Ray <rayhaan.bhikha@sky.uk>
2019-02-06 16:42:48 +0000

    WIP

--- thrift/src/main/thrift/user.thrift
+++ thrift/src/main/thrift/user.thrift
@@ -5,6 +5,7 @@ struct User {
     1: required string name
     2: required i16 age
     3: required string gender
+    4: required string faveColour
 }
 
 struct UsersListResponse {
--- user-gateway/src/main/scala/com/rayhaan/Routes/Users.scala
+++ user-gateway/src/main/scala/com/rayhaan/Routes/Users.scala
@@ -15,7 +15,17 @@ class Users extends Service[Request, Response] {
   override def apply(request: Request): Future[Response] = userDatastore.getUsers().map(transformResponse)
 
   def transformResponse(usersListResponse: UsersListResponse): Response = {
+
     val users: Seq[User] = usersListResponse.users
+
+    /*
+    *
+    *  Users should be map to a case class we define in this service.
+    *  This way we have more control whats coming in from the thrift service and can throw errors
+    *  if the api changes for instance or just ignore fields we might not want.
+    *
+    */
+
     val json: JValue = Extraction.decompose(users)
 
     val response = Response(Status.Ok)
